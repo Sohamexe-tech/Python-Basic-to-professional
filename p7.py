019fc727b6a0d90e413ee6f1a49710ddd32e05f0
Soham Dalvi <sohamdalvi52@gmail.com>
2025-12-08 18:28:43 +0530

    Add files via upload
    In this program we learned how to read radius and print Area and Circumference of circle

---
+++ p7.py
@@ -0,0 +1,9 @@
+#wapp to read radius and print area and cercumference of circle 
+
+radius=float(input("enter radius="))
+
+area=3.14*radius*radius
+circumference=2*3.14*radius
+
+print("area of circle",round(area,2))
+print("circumference of circle",round(circumference,2))
