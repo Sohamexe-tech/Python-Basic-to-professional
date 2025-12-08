0f0378089b5b27903e0422dbaaa8a2d7db8b50eb
Soham Dalvi <sohamdalvi52@gmail.com>
2025-12-08 18:35:38 +0530

    Add files via upload
    In this program we will learn how to read temprature in celcius and convert it into farenheit

---
+++ p11.py
@@ -0,0 +1,8 @@
+#wapp to read temprature ani celcius and convert it into farenheit
+
+
+celcius=float(input("enter tempturature in celcius"))
+
+farenheit=(celcius*1.8)+32
+
+print("farenheit",round(farenheit,2))
