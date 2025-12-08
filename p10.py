44163c74667c94191d2028b293b2252a160efd2e
Soham Dalvi <sohamdalvi52@gmail.com>
2025-12-08 18:34:11 +0530

    Add files via upload
    In this program we are able to learn how to convert Inr to dollar  , 1$=90rs

---
+++ p10.py
@@ -0,0 +1,8 @@
+#wapp to convert Inr to dollar  , 1$=90rs
+
+Rupees=float(input("Enter the amount in Rupees(₹)="))
+
+Dollar= Rupees/90
+
+print("Dollar= ",round(Dollar,2))
+print("$=",round(Dollar,2)) 
