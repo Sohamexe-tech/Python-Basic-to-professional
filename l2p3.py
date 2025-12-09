89e13d9f315941a3657160e5b4820a21b41bd7ff
Soham Dalvi <sohamdalvi52@gmail.com>
2025-12-09 15:35:07 +0530

    Add files via upload
    In this program we are able to ask for name and if the user enters the name then welcome them

---
+++ l2p3.py
@@ -0,0 +1,8 @@
+#wapp to as for name and if the user enters the name then welcome 
+
+name =input("Enter Name ")
+if  name=="":
+	print ("sorry You have not entered any name ")
+else:
+	msg="Good Morning "+ name
+print("msg=", msg)
