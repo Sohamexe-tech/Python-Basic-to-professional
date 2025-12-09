7392cff29bd0dba782715002474f30028df0f984
Soham Dalvi <sohamdalvi52@gmail.com>
2025-12-09 15:45:15 +0530

    Add files via upload
    In this program we are able to understand how to read rating betweeb 1 to 5 and print 
    #excellent if the rating is 5/4 , OK if the rating is 3/2 , else poor

---
+++ l2p9.py
@@ -0,0 +1,27 @@
+#wapp o read reating betweeb 1 to 5 and print 
+#excellent if the rating is 5/4 , ok if the rating is 3/2,  else poor
+
+Rating=float(input("Enter rating= "))
+
+if Rating<1:
+	print("Invalid Rating")
+
+elif Rating>5:
+	print("Invalid Rating")
+
+elif Rating==5:
+	print("Excellent")
+
+elif Rating==4:
+	print("Excellent")
+
+elif Rating==3:
+	print("OK")
+
+elif Rating==2:
+	print("OK")
+
+elif Rating==1:
+	print("Poor")
+
+	
