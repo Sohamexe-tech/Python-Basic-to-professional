f1eee6f0bb584a0d87a7e8aff7968e929529efff
Soham Dalvi <sohamdalvi52@gmail.com>
2025-12-11 17:42:42 +0530

    Add files via upload

---
+++ Lecture3/l3p3.py
@@ -0,0 +1,14 @@
+#wapp to read Four number and print minimum and maximum of Four numbers using built in functions
+
+n1=float(input("Enter the First number= "))
+n2=float(input("Enter the Second number= "))
+n3=float(input("Enter the Third number= "))
+n4=float(input("Enter the forth number= "))
+
+
+minres=min(n1,n2,n3,n4)
+maxres=max(n1,n2,n3,n4)
+
+
+print("Minimum result=",minres)
+print("Maxmum result=",maxres)
