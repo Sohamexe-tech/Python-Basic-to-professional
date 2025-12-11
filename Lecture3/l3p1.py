75e263a20b9017d14f33a0c0cd8d9173f8f69533
Soham Dalvi <sohamdalvi52@gmail.com>
2025-12-11 17:41:24 +0530

    Add files via upload

---
+++ Lecture3/l3p1.py
@@ -0,0 +1,9 @@
+#wapp to reae two integers and print minimum of two numbers
+
+n1=int(input("Enter the First number= "))
+n2=int(input("Enter the Second number= "))
+
+if n1<n2:
+	print("Minimum = ",n1)
+else:
+	print("Minimum = ",n2)
