ab0c87e2692b76f042333a5d96f50a9c86c0847e
Soham Dalvi <sohamdalvi52@gmail.com>
2026-01-10 18:14:46 +0530

    Add files via upload

---
+++ Lecture25/p6.py
@@ -0,0 +1,34 @@
+class Queue:
+    def __init__(self):
+            self.data=[]
+
+    def enqueue(self,ele):
+          self.data.append(ele)
+          self.data.sort()
+
+    def dequeue(self):
+          if len(self.data)==0:
+                return"Queue is Empty"
+          else:
+                return self.data.pop(0)
+          
+    def show(self):
+          print(self.data)
+
+q=Queue()
+while True:
+            op=int(input("1. Enqueue, 2.Dequeue, 3.Show , 4. Exit \n"))
+            if op==1:
+                ele=input("Enter Element: ")
+                q.enqueue(ele)
+            elif op==2:
+                print(q.dequeue())
+            elif op==3:
+                q.show()
+            elif op==4:
+                break
+            else:
+                print("Inavlid option 1")
+
+        
+    
