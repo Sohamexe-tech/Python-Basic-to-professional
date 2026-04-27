a9ffa70318950dc3f681876404fe9b3c9bfb0fe0
Soham Dalvi <sohamdalvi52@gmail.com>
2026-04-27 16:15:08 +0530

    Delete Lecture6 directory

--- Lecture6/p1.py
+++
@@ -1,9 +0,0 @@
-#wapf to print addition of two numbers 
-
-def add(n1,n2):
-	res=n1+n2
-	print ("res=",res)
-
-num1=float(input("enter first number "))
-num2=float(input("enter second number "))
-add(num1,num2)
