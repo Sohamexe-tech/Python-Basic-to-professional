41ca0cbd506f9c805e07927fb5a613ee90eac0cb
Soham Dalvi <sohamdalvi52@gmail.com>
2026-04-27 16:14:16 +0530

    Delete Lecture24 directory

--- Lecture24/p1.py
+++
@@ -1,8 +0,0 @@
-#sp for even odd programming 
-
-num=int(input("Enter the no. : "))
-
-if num%2==0:
-    print("no. is even" )
-else:
-    print("no. is odd")
--- Lecture24/p4.py
+++
@@ -1,8 +0,0 @@
-#wap to find square of a no.
-
-num = int(input("Enter the no: "))
-
-print("num= ",num)
-
-square=num**2
-print("Square= ",square)
